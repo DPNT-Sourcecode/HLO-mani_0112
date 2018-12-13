fdbb5c48b6201169f7b31a51ed511e1186237cfc
sbx_user1060 <sbx_user1060@ip-10-129-210-134.eu-west-2.compute.internal>
2018-12-13 15:10:55 +0000

    Thu Dec 13 15:10:55 UTC 2018

--- runDockerContainer.sh
+++ runDockerContainer.sh
@@ -8,8 +8,17 @@ docker images -f dangling=true -q | xargs -I {} docker rmi {}
 
 mkdir -p localstore
 
+echo "Run the below inside the container:"
+echo ""
+echo "    ./record/record-and-upload --config \$(pwd)/config/credentials.config --store \$(pwd)/localstore"
+echo " or"
+echo "    ./record/jre/bin/java -jar \$(pwd)/record/record-and-upload-0.0.16-linux.jar --config \$(pwd)/config/credentials.config --store \$(pwd)/localstore"
+echo ""
+
 docker run -it \
            --rm  \
-           --volume ${PWD}:/current-folder \
-           --workdir /current-folder \
+           --volume ${PWD}:/record-linux-packr    \
+           --volume ${PWD}/localstore:/localstore \
+           --volume ${PWD}/config:/config         \
+           --workdir /record-linux-packr          \
            ubuntu:18.04 /bin/bash
