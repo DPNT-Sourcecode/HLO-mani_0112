7a0d96ccda65260e78665c97b672764b78b24205
sbx_user1059 <sbx_user1059@ip-10-129-180-137.eu-west-2.compute.internal>
2019-01-08 22:12:46 +0000

    Tue Jan 08 22:12:46 UTC 2019

--- src/main/scala/befaster/solutions/HLO/Hello.scala
+++ src/main/scala/befaster/solutions/HLO/Hello.scala
@@ -3,5 +3,6 @@ package befaster.solutions.HLO
 import befaster.runner.SolutionNotImplementedException
 
 object Hello {
-  def hello(friendName: String): String = throw new SolutionNotImplementedException()
+  def hello(friendName: String): String = return friendName
 }
+
